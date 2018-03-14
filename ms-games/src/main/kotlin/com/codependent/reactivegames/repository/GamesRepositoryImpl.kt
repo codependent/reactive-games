32113d6b45feecbf5d923555c52861c02a69630c
Jose <jainigo@profile.es>
2018-03-14 13:39:50 +0100

    resilience4j

--- ms-games/src/main/kotlin/com/codependent/reactivegames/repository/GamesRepositoryImpl.kt
+++ ms-games/src/main/kotlin/com/codependent/reactivegames/repository/GamesRepositoryImpl.kt
@@ -7,12 +7,17 @@ import reactor.core.publisher.Flux
 @Service
 class GamesRepositoryImpl : GamesRepository {
 
-    override fun findAll() = Flux.just(
-            Game("The Secret of Monkey Island"),
-            Game("The Secret of Monkey Island 2"),
-            Game("The Secret of Monkey Island 3"),
-            Game("Loom"),
-            Game("Maniac Mansion"),
-            Game("Day of the Tentacle")).log()
-
+    override fun findAll(): Flux<Game> {
+        return if (Math.random() <= 5.0) {
+            Flux.error(RuntimeException("fail"))
+        } else {
+            Flux.just(
+                    Game("The Secret of Monkey Island"),
+                    Game("The Secret of Monkey Island 2"),
+                    Game("The Secret of Monkey Island 3"),
+                    Game("Loom"),
+                    Game("Maniac Mansion"),
+                    Game("Day of the Tentacle")).log()
+        }
+    }
 }
