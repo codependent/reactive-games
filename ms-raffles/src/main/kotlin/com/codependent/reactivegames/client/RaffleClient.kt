a351691f7cfdf1f3b2afac56d8a663e233d33152
Jose <joseantonio.inigo@gmail.com>
2018-03-17 14:12:11 +0100

    resilience4j 0.13.0-SNAPSHOT client cleanup

--- ms-raffles/src/main/kotlin/com/codependent/reactivegames/client/RaffleClient.kt
+++ ms-raffles/src/main/kotlin/com/codependent/reactivegames/client/RaffleClient.kt
@@ -4,29 +4,19 @@ import com.codependent.reactivegames.dto.Game
 import com.codependent.reactivegames.dto.Player
 import com.codependent.reactivegames.dto.RaffleResult
 import io.github.resilience4j.circuitbreaker.CircuitBreaker
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
-import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
-import io.github.resilience4j.circuitbreaker.autoconfigure.CircuitBreakerProperties
 import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
-import java.time.Duration
 import java.util.function.BiFunction
 
 @Component
 class RaffleClient(private val webClientBuilder: WebClient.Builder,
-                   private val circuitBreakerRegistry : CircuitBreakerRegistry) {
+                   private val circuitBreakerRegistry: CircuitBreakerRegistry) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
-    /*private val circuitBreakerConfig: CircuitBreakerConfig = CircuitBreakerConfig.custom()
-            .failureRateThreshold(50f)
-            .waitDurationInOpenState(Duration.ofMillis(10000))
-            .ringBufferSizeInHalfOpenState(5)
-            .ringBufferSizeInClosedState(5)
-            .build()*/
     private var gamesCircuitBreaker: CircuitBreaker = circuitBreakerRegistry.circuitBreaker("gamesCircuitBreaker")
     private var playersCircuitBreaker: CircuitBreaker = circuitBreakerRegistry.circuitBreaker("playersCircuitBreaker")
 
@@ -34,21 +24,19 @@ class RaffleClient(private val webClientBuilder: WebClient.Builder,
             getGames(), getPlayers(),
             BiFunction<Game, Player, RaffleResult> { g, p -> RaffleResult(g, p) }).log()
 
-    private fun getGames() : Flux<Game> {
+    private fun getGames(): Flux<Game> {
         logger.info("Circuit breaker ${gamesCircuitBreaker.name} state ${gamesCircuitBreaker.state}")
         return webClientBuilder.baseUrl("http://localhost:8081/api/v1").build()
                 .get().uri("/games").retrieve().bodyToFlux(Game::class.java)
                 .transform(CircuitBreakerOperator.of(gamesCircuitBreaker))
-                .onErrorResume(CircuitBreakerOpenException::class.java, { _ -> Flux.empty() })
                 .onErrorResume(Exception::class.java, { _ -> Flux.empty() })
     }
 
-    private fun getPlayers() : Flux<Player> {
+    private fun getPlayers(): Flux<Player> {
         logger.info("Circuit breaker ${playersCircuitBreaker.name} state ${playersCircuitBreaker.state}")
         return webClientBuilder.baseUrl("http://localhost:8082/api/v1").build()
                 .get().uri("/players").retrieve().bodyToFlux(Player::class.java)
                 .transform(CircuitBreakerOperator.of(playersCircuitBreaker))
-                .onErrorResume(CircuitBreakerOpenException::class.java, { _ -> Flux.empty() })
                 .onErrorResume(Exception::class.java, { _ -> Flux.empty() })
     }
 }
